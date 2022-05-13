4c308a7a8b3268ea4fa06727291d5298d986e78f
Felipe Araújo <felipe.jra@gmail.com>
2022-05-13 10:22:42 -0300

    Customizing plot

--- src/nlp/visualization.py
+++ src/nlp/visualization.py
@@ -2,40 +2,36 @@ import networkx as nx
 from mycolorpy import colorlist as mcp
 import matplotlib.pyplot as plt
 
-# Cada ponto central será um dos 5 termos de maior TF-IDF. 
-# As conexões são as palavras próximas obtidas em 4.5. 
+# Cada ponto central será um dos 5 termos de maior TF-IDF.
+# As conexões são as palavras próximas obtidas em 4.5.
 
-words_and_connections = {"fruit": {"weight": 4, "neighbors": ["apple", "orange", "banana"]}, \
-    "city": {"weight": 2, "neighbors": ["São Paulo", "Paris", "Rome"]}, \
-    "language": {"weight": 10, "neighbors": ["Python", "Java", "C#", "Ruby"]}}
-nodes = []
+words_and_connections = {"fruit": {"weight": 4, "neighbors": ["apple", "orange", "banana"]},
+                         "city": {"weight": 2, "neighbors": ["São Paulo", "Paris", "Rome"]},
+                         "language": {"weight": 10, "neighbors": ["Python", "Java", "C#", "Ruby"]}}
+
 egdes = []
 
 g = nx.Graph()
-colors = color1=mcp.gen_color(cmap="winter",n=len(words_and_connections))
+colors = color1 = mcp.gen_color(cmap="Set1", n=len(words_and_connections))
+#print(colors)
+
 for word, color in zip(words_and_connections, colors):
-    #nodes.append(word)
     for n in words_and_connections[word]["neighbors"]:
-        #nodes.append(n)
         g.add_edge(word, n, weight=1)
         g.nodes[n]['weight'] = 300
-        g.nodes[n]['corlo'] = color
+        g.nodes[n]['color'] = color
     g.nodes[word]['weight'] = words_and_connections[word]['weight']*300
+    g.nodes[word]['color'] = color
 
-plt.figure(figsize=(10,10))
-
-edge_weight = list(nx.get_edge_attributes(g,'weight').values())
-node_weight = list(nx.get_node_attributes(g,'weight').values())
-
-nx.draw_networkx(g, width=edge_weight, node_size=node_weight)
+node_colors = [g.nodes[word]['color'] for word in g.nodes]
 
 
-#g.add_nodes_from(nodes)
+plt.figure(figsize=(10, 10))
 
-#subax1 = plt.subplot(121)
-#nx.draw(g, with_labels=True, font_weight='bold')
-#subax2 = plt.subplot(122)
-#nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+edge_weight = list(nx.get_edge_attributes(g, 'weight').values())
+node_weight = list(nx.get_node_attributes(g, 'weight').values())
+
+nx.draw_networkx(g, width=edge_weight, node_size=node_weight, edge_color='#b5b1b0', node_color=node_colors, font_family = 'Ubuntu')
 
 # Se necessario, instalar o Tk:
 # UserWarning: Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure.
