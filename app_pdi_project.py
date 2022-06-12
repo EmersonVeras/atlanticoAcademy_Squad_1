9148ee77c815d0321813e29fa49f4a2a40873136
Romulo Castro <romulodecastropm2@gmail.com>
2022-06-11 23:18:19 -0300

    calculate IOU metric resolved #19

--- app_pdi_project.py
+++ app_pdi_project.py
@@ -4,6 +4,7 @@ from turtle import title
 import cv2 as cv
 from src.pdi.file_utils import list_images
 from src.pdi.generate_metrics import segment
+from src.pdi.generate_metrics import calculate_iof
 from src.pdi.plot_utils import plot_images
 from src.pdi.get_dims import get_dimensions
 import matplotlib.pyplot as plt
@@ -18,13 +19,14 @@ def main():
     
     target = intersection[0]
     img = cv.imread("data/pdi/Raw imgs/" +  target)
-    golden_pattern = plt.imread("data/pdi/golden_patterns/" +  target).astype(int)
+    golden_pattern = plt.imread("data/pdi/golden_patterns/" +  target).astype(int)[:,:,0]
 
     segmented_images = segment(img)
 
     images = [img, golden_pattern]
     images.extend(segmented_images)
-    
+    iou_results = calculate_iof(golden_pattern, segmented_images)
+    print(iou_results)
     plot_images(images, ["Original", "Golden Pattern", "Chan vese", "Otsu", "KMeans"])
 
 
--- src/pdi/generate_metrics.py
+++ src/pdi/generate_metrics.py
@@ -3,9 +3,22 @@ import matplotlib.pyplot as plt
 from .chan_vese import chan_vese_segmentation
 from .otsu import otsu_segmentantion
 from .kmeans import kmeans_segmentation
+import numpy as np
+from src.pdi.plot_utils import plot_images
 
 def segment(image):
     technique_01 = chan_vese_segmentation(image)
     technique_02 = otsu_segmentantion(image)
     technique_03 = kmeans_segmentation(image)
     return [technique_01, technique_02, technique_03]
+
+
+def calculate_iof(golden_pattern, comparatives):
+    iou_results = []
+    for comparative in comparatives:
+        intersection = np.logical_and(golden_pattern, comparative)
+        union = np.logical_or(golden_pattern, comparative)
+
+        iou_results.append(np.sum(intersection)/np.sum(union))
+
+    return iou_results
