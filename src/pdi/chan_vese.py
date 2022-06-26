6f5e8c794eb6547f7140a85b6687802b8219223b
Felipe Araújo <felipe.jra@gmail.com>
2022-06-26 09:29:44 -0300

    Invert final segmentation in Chan Vese when needed

--- src/pdi/chan_vese.py
+++ src/pdi/chan_vese.py
@@ -3,13 +3,21 @@
 from skimage.segmentation import chan_vese
 import cv2
 
-
-def chan_vese_segmentation(image):
-    #image = img_as_float(image)
+def chan_vese_segmentation(image):    
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv = chan_vese(image, mu=0.25, lambda1=1, lambda2=1, tol=1e-3,
                max_num_iter=200, dt=0.5, init_level_set="checkerboard",
                extended_output=True)
     # as last step, apply a limiar to the segmented image
+
     segmented = cv[0] > 0.5
+    invert = False
+    if segmented.mean() > 0.5:
+        invert = True
+    if invert:
+        print('Inverting image in chan vese')
+        segmented = ~segmented
+
+    
+    # invert segmentation when needed
     return segmented
